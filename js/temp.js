63a8d49d0c329709b45fcbc028dcf43f171ea370
Isaac Nkoy <nkoylokoteisaacdelor@gmail.com>
2019-07-05 14:02:38 +0200

    javascript change

--- js/temp.js
+++ js/temp.js
@@ -1,15 +1,17 @@
-function myFunction(){
+document.getElementById('weatherApp').addEventListener('submit', function(e) {
+    e.preventDefault();
     var temperature = document.getElementById("temp").value;
 
 
-    if(temperature >= 25){
-        alert("Wear t-shirt, shorts and flip flops");
-    }else if(temperature>= 11 && temperature <= 15){
-        alert("wear a wind breaker a rain coat or a jacket");
+    if(temperature >= 17 && temperature <= 40){
+        document.getElementById("text").innerHTML = "Wear t-shirt, shorts and flip flops";
+    }else if(temperature >= 11 && temperature <= 16 ){
+        document.getElementById("text").innerHTML = "wear a wind breaker a rain coat or a jacket";
 
-    }else if(temperature >=-10 && temperature<=-1){
-        alert("dress warmly it's ice cold")
-    }
+    }else if(temperature >= 0 && temperature <= 10){
+        document.getElementById("text").innerHTML = "dress warmly it's ice cold";
+    }else{
+        document.getElementById("text").innerHTML = "invalid temperature";
+    }    
+});
 
-
-}
